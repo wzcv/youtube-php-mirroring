150a550f3469623df3789545ed721fbcee23b83f
wukes <zxqsss@10086.ga>
2016-07-11 21:24:44 +0800

    Update info.php

--- inc/info.php
+++ inc/info.php
@@ -1,6 +1,6 @@
 <?php
 //
-$ym='http://'.$_SERVER['SERVER_NAME'];//你的站点域名。不可为空，否则解析器无法工作
+$ym='http://'.$_SERVER['SERVER_NAME'].'/';//你的站点域名。不可为空，否则解析器无法工作
 $parser='parser';//解析器目录,默认parser，如果本程序不是安装在根目录请加上上级路径，如abc/parser
 $home_v='UCcLK3j-XWdGBnt5bR9NJHaQ';//首页推荐视频用户身份标示
 $youtube_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//youtube视频API
