9303fd1c7a9d606512e443d510f72c3744cfbb0d
wukes <zxqsss@10086.ga>
2016-07-11 18:41:01 +0800

    Update thumbnail.php

--- thumbnail.php
+++ thumbnail.php
@@ -1,8 +1,16 @@
 <?php
 $vid=$_GET['vid'];
-
+function fcurl($url){
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$data = curl_exec($ch);
+curl_close($ch);
+return $data;
+}
 header('content-type:image/jpg;');
 $vidimg='https://i.ytimg.com/vi/'.$vid.'/mqdefault.jpg';
-$img=file_get_contents($vidimg);
+$img=fcurl($vidimg);
 echo $img;
 ?>
