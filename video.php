0a50eaff4a31028b5d8d3f1acaeb3a47f8eb802c
wukes <zxqsss@10086.ga>
2016-07-11 21:41:52 +0800

    Update video.php

--- video.php
+++ video.php
@@ -147,7 +147,7 @@ $video_list = json_decode(fcurl($jsonurl));
 $video_list1=object_array($video_list);
 ?>
 <script src="js/jquery.js"></script>
-<script src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="//cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <div class="wrapper container">
  <div  class="pull-left" id="a1" style="width:850px">
      <!--ckplayer配置开始-->
