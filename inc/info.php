5621f617939b5df2fe8363d01534f04d9930eef0
wukes <zxqsss@10086.ga>
2016-07-11 18:15:16 +0800

    Update info.php

--- inc/info.php
+++ inc/info.php
@@ -1,11 +1,11 @@
-﻿<?php
+<?php
 //
-$ym='http://abc.net/';//你的站点域名。不可为空，否则解析器无法工作
+$ym='http://'.$_SERVER['SERVER_NAME'];//你的站点域名。不可为空，否则解析器无法工作
 $parser='parser';//解析器目录,默认parser，如果本程序不是安装在根目录请加上上级路径，如abc/parser
 $home_v='UCcLK3j-XWdGBnt5bR9NJHaQ';//首页推荐视频用户身份标示
-$youtube_api='你的api';//youtube视频API
-$gurl_api='你的api';//谷歌缩址api，用于防止dns污染
-$title='Youtube镜像';//您的网站名字
+$youtube_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//youtube视频API
+$gurl_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//谷歌缩址api，用于防止dns污染
+$title='Youtube直通车';//您的网站名字
 
 //seo设置
 $keyw_ords='Youtube镜像,google,youtube,镜像';//keywords头部关键词设置
