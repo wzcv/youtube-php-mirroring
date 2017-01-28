bf0239263b28a937890fb518107b2076a679f960
wzcv <qhwuzhichen@126.com>
2017-01-28 20:44:35 +0800

    Update info.php

--- inc/info.php
+++ inc/info.php
@@ -3,8 +3,8 @@
 $ym='http://'.$_SERVER['SERVER_NAME'].'/';//你的站点域名。不可为空，否则解析器无法工作
 $parser='parser';//解析器目录,默认parser，如果本程序不是安装在根目录请加上上级路径，如abc/parser
 $home_v='UCcLK3j-XWdGBnt5bR9NJHaQ';//首页推荐视频用户身份标示
-$youtube_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//youtube视频API
-$gurl_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//谷歌缩址api，用于防止dns污染
+$youtube_api='AIzaSyDikY9yfObZcKxk4VE09h3OmR8lL0VQTpc';//youtube视频API
+$gurl_api='AIzaSyBe6Dxb40N5rI4BCEB24ZKq7wm4ZWzObxo';//谷歌缩址api，用于防止dns污染
 $title='Youtube直通车';//您的网站名字
 
 //seo设置
