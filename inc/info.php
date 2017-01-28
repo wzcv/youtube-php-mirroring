4c9ea024deb8a9ae421ffd0d65ce06e339ab1095
wzcv <qhwuzhichen@126.com>
2017-01-28 20:53:01 +0800

    Update info.php

--- inc/info.php
+++ inc/info.php
@@ -1,6 +1,6 @@
 <?php
 //
-$ym='https://wzcv.herokuapp.com/';//你的站点域名。不可为空，否则解析器无法工作
+$ym='https://wzce.000a.biz/youtube/';//你的站点域名。不可为空，否则解析器无法工作
 $parser='parser';//解析器目录,默认parser，如果本程序不是安装在根目录请加上上级路径，如abc/parser
 $home_v='UCcLK3j-XWdGBnt5bR9NJHaQ';//首页推荐视频用户身份标示
 $youtube_api='AIzaSyDikY9yfObZcKxk4VE09h3OmR8lL0VQTpc';//youtube视频API
